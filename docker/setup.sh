2c0c7b2fcecd75829cd15dc78dd45aef740f0e1e
A.J. Hunyady <aj@infinyon.com>
2024-10-25 15:37:07 -0700

    Add Support for Docker (#50)
    * add shutdown
    
    * wip
    
    * docker: docker managed vols
    
    * docker: fix networking for sdf-worker
    
    * docker: update docs
    
    * update dockerfile
    
    * update dockerfile & readme
    
    * update docker setion
    
    * remove .gitignore
    
    ---------
    
    Co-authored-by: Alan Chen <alan@infinyon.com>
    Co-authored-by: Sehyo Chang <sehyo@fluvio.io>

---
+++ docker/setup.sh
@@ -0,0 +1,8 @@
+# Run SDF in Docker
+docker compose up -d
+
+# Download profile to the CLI
+fluvio profile add sdf-docker 127.0.0.1:9103 docker
+
+# Create a worker profile
+sdf worker register docker-worker sdf-worker
