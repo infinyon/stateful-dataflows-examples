c7eba47bc43a53463743b6c710bd7baf98a10944
A.J <aj@infinyon.com>
2024-11-07 07:30:45 -0800

    update connector version

--- dataflows/car-processing/run.sh
+++ dataflows/car-processing/run.sh
@@ -1,8 +1,8 @@
 fluvio cluster start
 fluvio cluster status
 cd /workspace/connectors/ 
-cdk deploy start --ipkg infinyon-http-source-0.3.8.ipkg -c license-connector.yaml
-cdk deploy start --ipkg infinyon-http-source-0.3.8.ipkg -c car-connector.yaml
+cdk deploy start --ipkg infinyon-http-source-0.4.3.ipkg -c license-connector.yaml
+cdk deploy start --ipkg infinyon-http-source-0.4.3.ipkg -c car-connector.yaml
 cd /workspace
 
 sdf clean
